16a072e7a468d3d94c04afc3cad6320b17de3cd5
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-21 01:53:23 +0000

    Automatically backed up by Learn

--- db/migrate/20191121014742_create_movies.rb
+++ db/migrate/20191121014742_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table :costumes do |t|
+    create_table :movies do |t|
       t.string :name
       t.integer :price
       t.string :image_url
